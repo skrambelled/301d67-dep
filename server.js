7c319d242b98d445466f95242bc56ba8abb405c3
Mark Bell <skrambelled@gmail.com>
2020-10-09 11:10:12 -0700

    init setup

---
+++ server.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('./public'));
+
+app.get('/', (request, response) => {
+  response.sendFile('./public/index.html');
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`)
+});
